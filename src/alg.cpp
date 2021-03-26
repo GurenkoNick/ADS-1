fbf824234a22fd2856fcfc3142da1aea88a1b879
GurenkoNick <78843007+GurenkoNick@users.noreply.github.com>
2021-03-26 15:41:18 +0300

    Гуренко Никита

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int i = 0; count = 0;
+  int j = size - 1;
+  while (i < j) {
+    int mid = i + (j - i) / 2;
+    if (arr[mid] == value)
+      ++count;
+    else if (arr[mid] > value)
+      j = mid;
+    else 
+      i = mid + 1;
+  }
+  if (count > 0)
+    return count;
+  else
+    return 0;
 }
