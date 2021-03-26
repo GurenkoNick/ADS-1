94bd82f31975a3ef7d38f717a72e3283e8a0124f
GurenkoNick <78843007+GurenkoNick@users.noreply.github.com>
2021-03-26 17:41:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ int cbinsearch(int* arr, int size, int value) {
         int mid = i + (j - i) / 2;
         if (arr[mid] >= value)
             j = mid;
-        else 
+        else
             i = mid + 1;
         if (arr[i] == value) {
             while (arr[i] == value) {
